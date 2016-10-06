770192c66d8cc5d07ee902daf2b8b2c53b86694b
Andrey Matin <andrey.matin@techimg.com>
2016-10-06 23:27:41 +0300

    some updates

--- index.php
+++ index.php
@@ -1,25 +1,30 @@
 <?php
 	// Get action parameter
 	$request_uri = $_SERVER['REQUEST_URI'];
-	$request_uri_data = explode("/",$_SERVER['REQUEST_URI']);
+	$request_uri_data = explode('/', $request_uri);
+	
+	// Action parameter for / 
 	$action = $request_uri_data['1'];
+	
+	// Local full path
 	$local_dir = dirname(__FILE__);
 
 	/**
-	 * Libs
+	 * Load Libs
 	 */ 
+
 	// PHP Mailer
-	require $local_dir . '/lib/phpmailer/PHPMailerAutoload.php';
+	require_once $local_dir . '/lib/phpmailer/PHPMailerAutoload.php';
 	
 	// Mustache Template Engine
-	require $local_dir . '/lib/mustache/src/Mustache/Autoloader.php';
+	require_once $local_dir . '/lib/mustache/src/Mustache/Autoloader.php';
 
 	/**
 	 * Modules
 	 */ 
 
 	// Contact Form
-	require $local_dir . '/mod/email.php';
+	require_once $local_dir . '/mod/email.php';
 	
 	// Templates
-	require $local_dir . '/mod/mustache.php';
+	require_once $local_dir . '/mod/mustache.php';
